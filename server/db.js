3edee155cc635505a30474e4cddcbeea55d41cf1
Kevin Ung <ungkevin16@gmail.com>
2026-03-22 19:26:34 -0700

    return to ipv6 use for db connection

--- server/db.js
+++ server/db.js
@@ -4,9 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
-    checkServerIdentity: () => undefined,
   },
-  family: 4,
 });
 
 module.exports = pool;
